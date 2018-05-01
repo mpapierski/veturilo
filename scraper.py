b00fba830f24ee6b35b55a43dbf2a5775d77ebe2
Michał Papierski <michal@papierski.net>
2018-05-01 12:19:32 +0200

    Initial commit.

---
+++ scraper.py
@@ -0,0 +1,77 @@
+from requests_html import HTMLSession
+from datetime import datetime
+import pytz
+import decimal
+import os
+import csv
+import sys
+
+
+tz = pytz.timezone('Europe/Warsaw')
+
+session = HTMLSession()
+
+
+veturilo_user = os.environ['VETURILO_USER']
+veturilo_pass = os.environ['VETURILO_PASS']
+
+
+def parse_value(value):
+    """Parses delta value.
+
+    "-10.00" -> Decimal('-10.00')
+    "+10.00" -> Decimal('10.00')
+    "" -> None
+    """
+    try:
+        return decimal.Decimal(value)
+    except decimal.InvalidOperation:
+        return None
+
+
+r = session.get('https://www.veturilo.waw.pl/veturilo-login/')
+r.raise_for_status()
+r.html.render()
+
+r = session.get('https://poland.nextbike.net/iframe/?domain=vp&L=pl&id=login&redirect_index=https://www.veturilo.waw.pl/veturilo-login&redirect_account=https://www.veturilo.waw.pl/account')
+r.raise_for_status()
+r.html.render()
+
+form = r.html.find('#mailform', first=True)
+if form is None:
+    raise RuntimeError('Unable to find form')
+
+r = session.post('https://poland.nextbike.net/iframe/?id=login&L=pl&domain=vp', data={
+    'logintype': 'login',
+    'redirect_index': 'https://www.veturilo.waw.pl/veturilo-login',
+    'redirect_account': 'https://www.veturilo.waw.pl/account',
+    'user': veturilo_user,
+    'pass': veturilo_pass,
+    'submit': 'Zaloguj'
+})
+r.raise_for_status()
+
+r = session.get('https://poland.nextbike.net/iframe/?L=pl&domain=vp')
+r.raise_for_status()
+table = r.html.find('#contenttable_account', first=True)
+if table is None:
+    raise RuntimeError('Unable to find table')
+
+total = decimal.Decimal('0')
+
+with open('rentals.csv', 'w') as rentals:
+    writer = csv.writer(rentals)
+
+    for row in table.find('tr:has(td)'):
+        cells = row.find('td')
+
+        date_text = cells[0].text.strip()
+        if not date_text:
+            continue
+        date_naive = datetime.strptime(date_text, '%Y-%m-%d %H:%M:%S')
+        date = tz.localize(date_naive)
+        description = cells[1].text.strip()
+        value = parse_value(cells[2].text.strip())
+        if value is not None:
+            total += value
+        writer.writerow([date, description, value])
