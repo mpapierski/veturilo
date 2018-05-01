b1dee303b73394511cb9f9642f6fb0964fc623e2
Michał Papierski <michal@papierski.net>
2018-05-01 20:31:24 +0200

    Extract rental details and print some stats.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 from requests_html import HTMLSession
-from datetime import datetime
+from datetime import datetime, time, timedelta
+from collections import defaultdict
+import operator
 import pytz
 import decimal
 import os
@@ -7,7 +9,7 @@ import csv
 import sys
 import codecs
 import re
-
+import statistics
 
 tz = pytz.timezone(os.getenv('VETURILO_TZ', 'Europe/Warsaw'))
 
@@ -44,6 +46,18 @@ def parse_row_id(value):
     return None
 
 
+def extract_rental_details(value):
+    """Extract rental details.
+
+    "Rower 63854 do 13:00:22" -> (63854, time(13, 00, 22))
+    """
+    match = re.search(r'^Rower (\d+) do (\d+):(\d+):(\d+)', value)
+    if match is None:
+        return None
+    bike_id, hours, minutes, seconds = map(int, match.groups())
+    return (bike_id, time(hours, minutes, seconds))
+
+
 def main():
 
     r = session.get('https://www.veturilo.waw.pl/veturilo-login/')
@@ -74,6 +88,10 @@ def main():
 
     total = decimal.Decimal('0')
 
+    rental_time = timedelta()
+    bikes = defaultdict(int)
+    rental_times = []
+
     with open('rentals.csv', 'w', encoding='utf-8') as rentals:
         rentals.write(codecs.BOM_UTF8.decode('utf-8'))
 
@@ -94,9 +112,39 @@ def main():
             date = tz.localize(date_naive)
             description = cells[1].text.strip()
             value = parse_value(cells[2].text.strip())
+
+            rental = extract_rental_details(description)
+            if rental is not None:
+                (bike_id, time_point) = rental
+                return_date = date_naive.replace(
+                    hour=time_point.hour,
+                    minute=time_point.minute,
+                    second=time_point.second)
+                # How long this bike have been rented
+                timediff = return_date - date_naive
+                # Add this
+                rental_times += [timediff]
+                # Mark this bike as rented
+                bikes[bike_id] += 1
+                # Add total rental time
+                rental_time += timediff
+
             if value is not None:
                 total += value
             writer.writerow([row_id, date, description, value])
+
+    print('Account status', total)
+    print('Total rental time', rental_time)
+    print('Longest rental time', max(rental_times))
+    print('Shortest rental time', min(rental_times))
+
+    rental_by_seconds = (td.total_seconds() for td in rental_times)
+    print('Average rental time', timedelta(
+        seconds=statistics.mean(rental_by_seconds)))
+    sorted_bikes = sorted(
+        bikes.items(), key=operator.itemgetter(1), reverse=True)
+    for bike_id, count in sorted_bikes:
+        print(bike_id, count)
 
 
 if __name__ == '__main__':
--- scraper_tests.py
+++ scraper_tests.py
@@ -1,5 +1,5 @@
 from unittest import mock, TestCase
-
+from datetime import time
 from decimal import Decimal
 
 
@@ -11,10 +11,10 @@ class ScraperTestCase(TestCase):
         'VETURILO_PASS': 'dupa.8'
     })
     def setUp(self):
-        from scraper import parse_value, parse_row_id
+        from scraper import parse_value, parse_row_id, extract_rental_details
         self.parse_value = parse_value
         self.parse_row_id = parse_row_id
-
+        self.extract_rental_details = extract_rental_details
 
     def test_positive(self):
         value = self.parse_value('+10.00')
@@ -41,3 +41,11 @@ class ScraperTestCase(TestCase):
 
     def test_parse_row_id_invalid(self):
         self.assertIsNone(self.parse_row_id('row_asdf'))
+
+    def test_extract_bike_details(self):
+        values = self.extract_rental_details(
+            'Rower 123456 do 14:15:16 (ul. Jana Pawła II)')
+        self.assertEquals(len(values), 2)
+        bike_id, time_point = values
+        self.assertEquals(bike_id, 123456)
+        self.assertEquals(time_point, time(14, 15, 16))
