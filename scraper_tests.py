8dcc3834110db1826af5abb76bf04604957a8dcf
Michał Papierski <michal@papierski.net>
2018-05-01 12:42:24 +0200

    Improved tests.

--- scraper_tests.py
+++ scraper_tests.py
@@ -1,16 +1,28 @@
+from unittest import mock, TestCase
+
 from decimal import Decimal
-from scraper import parse_value
-
-from nose.tools import assert_equals, assert_is_none
 
 
-def test_positive():
-    assert_equals(parse_value('+10.00'), Decimal('10.00'))
+class ScraperTestCase(TestCase):
 
+    @mock.patch.dict('os.environ', {
+        'VETURILO_TZ': 'Europe/Warsaw',
+        'VETURILO_USER': '+48123456789',
+        'VETURILO_PASS': 'dupa.8'
+    })
+    def setUp(self):
+        from scraper import parse_value
+        self.parse_value = parse_value
 
-def test_negative():
-    assert_equals(parse_value('-10.00'), Decimal('-10.00'))
+    def test_positive(self):
+        value = self.parse_value('+10.00')
+        self.assertIsNotNone(value)
+        self.assertEquals(value, Decimal('10.00'))
 
+    def test_negative(self):
+        value = self.parse_value('-10.00')
+        self.assertIsNotNone(value)
+        self.assertEquals(value, Decimal('-10.00'))
 
-def test_invalid():
-    assert_is_none(parse_value(''))
+    def test_invalid(self):
+        self.assertIsNone(self.parse_value(''))
